f9d8482cca45ae9d07c2a7dd440b9f715bdff93d
Dima Khutorov <96331923+dmkhu@users.noreply.github.com>
2023-01-22 17:55:08 +0300

    creat recomendations

---
+++ recomdation/scriptRecomendations.js
@@ -0,0 +1,14 @@
+//кнопка меню телефона
+function navPhone() {
+    let menu = document.querySelector(".phone-menu");
+    let menuStyle = window.getComputedStyle(menu);
+    let menuDisplay = menuStyle.getPropertyValue("display");
+    if(menuDisplay === "block") {
+        document.querySelector('.phone-menu').style= 'display: none';
+        document.querySelector('.context').style= 'display: block';
+    }
+    else {
+        document.querySelector('.context').style= 'display: none';
+        document.querySelector('.phone-menu').style= 'display: block';
+    }
+}
